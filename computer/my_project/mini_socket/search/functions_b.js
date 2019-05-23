9cff328334458d8d54f85cfeeeec2299938d832e
hexu <hexu_1985@sina.com>
2019-05-23 17:44:39 +0800

    add doc TCPSocket and UDPSocket for my_project/mini_socket

--- computer/my_project/mini_socket/search/functions_b.js
+++ computer/my_project/mini_socket/search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['tcpserversocket',['TCPServerSocket',['../class_mini_socket_1_1_t_c_p_server_socket.html#a55636e6f903f588cf13e6b6670756db7',1,'MiniSocket::TCPServerSocket']]],
+  ['tcpserversocket',['TCPServerSocket',['../class_mini_socket_1_1_t_c_p_server_socket.html#a12889283cbaa7e81e5f95490c56a17c5',1,'MiniSocket::TCPServerSocket::TCPServerSocket()=default'],['../class_mini_socket_1_1_t_c_p_server_socket.html#a55636e6f903f588cf13e6b6670756db7',1,'MiniSocket::TCPServerSocket::TCPServerSocket(const SocketAddress &amp;localAddress)']]],
   ['tcpsocket',['TCPSocket',['../class_mini_socket_1_1_t_c_p_socket.html#ae12b656795249d481e5b4c666851e700',1,'MiniSocket::TCPSocket::TCPSocket()=default'],['../class_mini_socket_1_1_t_c_p_socket.html#a6de74201abb5ea59d2b13cf261fb3f2d',1,'MiniSocket::TCPSocket::TCPSocket(const SocketAddress &amp;foreignAddress)']]],
   ['tostring',['toString',['../class_mini_socket_1_1_socket_address_view.html#ac0cc1a739da3966994d79ce08fef2d1b',1,'MiniSocket::SocketAddressView::toString()'],['../class_mini_socket_1_1_socket_address.html#aa8dac7fab80f0d3d3c5742773802c3a2',1,'MiniSocket::SocketAddress::toString()']]]
 ];
